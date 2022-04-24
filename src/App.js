33e835e6226d0821af1dcdfbab20fb96d9cd3718
RitikSinha <ritikk910@gmail.com>
2022-04-24 11:59:45 +0530

    major change

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import React from "react";
-import HomePage from "./pages/HomePage";
+
+import AppRating from "./components/AppRating";
+import BrandsLove from "./components/BrandsLove";
+import Footer from "./components/common/footer";
+import Header from "./components/common/Header";
+import CredExperience from "./components/CredExperience";
+import CredSecurity from "./components/CredSecurity";
+import CredStory from "./components/CredStory";
+import FeelSpecial from "./components/FeelSpecial";
+import HeroSection from "./components/HeroSection";
+import MobileScroll from "./components/MobileScroll";
+import ProductShowcase from "./components/ProductShowcase";
+import WindowPeak from "./components/WindowPeak";
 
 const App = () => {
-  return <HomePage />;
+  return (
+    <>
+      <Header />
+      <HeroSection />
+      <ProductShowcase />
+      <FeelSpecial />
+      <BrandsLove />
+      <CredExperience />
+      <MobileScroll />
+      <div className="non-mobile">
+        <WindowPeak />
+      </div>
+      <CredSecurity />
+      <CredStory />
+      <AppRating />
+      <Footer />
+    </>
+  );
 };
 
 export default App;
