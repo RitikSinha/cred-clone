268e443b7168256b2348b31ae921f8fa77d60de4
RitikSinha <ritikk910@gmail.com>
2022-09-16 08:18:49 +0530

    fixed scrool

--- src/components/ProductShowcase/index.js
+++ src/components/ProductShowcase/index.js
@@ -1,5 +1,4 @@
-import React, { useRef, useState } from "react";
-import { useEffect } from "react/cjs/react.development";
+import React, { useRef, useState, useEffect } from "react";
 import "./productShowcase.css";
 
 const ProductShowcase = () => {
